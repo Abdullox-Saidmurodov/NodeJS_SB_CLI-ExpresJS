eb826916e028e7ff4fe987ed805811ec64901f65
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-27 11:39:11 +0500

    3

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
+
+app.get('/', (req, res) => {
+    // res.send('Main page')
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    // res.send('About page')
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
 
 const PORT = process.env.PORT || 4100
 app.listen(4100, () => console.log(`Server is running on port: ${PORT}`)) 
